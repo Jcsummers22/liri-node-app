6cf98b090207e13e855e6ee191003d6ff23af6ba
Jason Summers <jcsummers04@gmail.com>
2017-09-27 21:18:59 -0700

    first commit, skeleton files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'gtpOsJTsKLfLjr5tBOk2NwTm8',
+  consumer_secret: '8tj296av4IwsgBWN6YjMqjXG9eBsY3Nuk81xD6lbWvvpF76eWO',
+  access_token_key: '736684406383906816-8pmkY1HU5xvOmOkR3iW4wC6Py5piBXp',
+  access_token_secret: 'vqP9wjRPJpV4voaGmKXHjOaUGzgK7ucpjg2YTvHSstw2j',
+}
+
+module.exports = twitterKeys;
